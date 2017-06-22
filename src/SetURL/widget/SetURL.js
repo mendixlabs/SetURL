4b8ae1ab3c6c7b3e4e928a0e70b605ace9163a92
Robert van 't Hof <robert.van.thof@mendix.com>
2017-06-22 14:08:46 +0200

    Cleaned up dependencies

--- src/SetURL/widget/SetURL.js
+++ src/SetURL/widget/SetURL.js
@@ -2,22 +2,9 @@ define([
 	"dojo/_base/declare",
 	"mxui/widget/_WidgetBase",
 
-	"mxui/dom",
-	"dojo/dom",
-	"dojo/dom-prop",
-	"dojo/dom-geometry",
-	"dojo/dom-class",
-	"dojo/dom-style",
-	"dojo/dom-construct",
-	"dojo/_base/array",
 	"dojo/_base/lang",
-	"dojo/text",
-	"dojo/html",
-	"dojo/_base/event",
-	"dojo/aspect",
-
-
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, aspect) {
+	"dojo/aspect"
+], function (declare, _WidgetBase, lang, aspect) {
 	"use strict";
 
 	return declare("SetURL.widget.SetURL", [_WidgetBase], {
